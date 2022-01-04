59a41e08a9b8d937e1468eb936ee7503b726c1e2
Sheng-yu Tsai <uopsdod@gmail.com>
2022-01-04 04:47:27 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,56 @@ const con = mysql.createConnection({
 });
 
 exports.handler = async (event) => {
-  await update_db();
-  return "Done...";
+    console.log("start processing");
+    for (var i = 0; i < event.Records.length ; i++) {
+        let record = event.Records[i]
+        let { body } = record;
+        console.log("body: ", body);
+        
+        console.log("type of body: " + (typeof body))
+        if (typeof body === 'string') {
+            body = JSON.parse(body)
+        }
+
+        let { boughtTicketId } = body;
+        let { agendaProvider } = body;
+        console.log("boughtTicketId: ", boughtTicketId);
+        console.log("agendaProvider: ", agendaProvider);
+        await delay(3000);
+        await update_db(boughtTicketId, agendaProvider);
+
+    }  
+    console.log("ends processing.");
+  
+  
+  return "Done.....";
 };
 
-function update_db() {
+function update_db(boughtTicketId, agendaProvider) {
   return new Promise(async (resolve, reject) => {
-    con.query("SELECT * FROM ticketsystem.ticket", function (err, result) {
+    if (agendaProvider == 'JSDC') {
+      console.log("bank 10%, host 50%, subhost 20%, platform 20%");
+    }else {
+      console.log("bank 15%, host 45%, subhost 25%, platform 15%");
+    }
+    
+    let ticket_id = '1';
+    let sql = `UPDATE ticketsystem.ticket SET ticket_status = 'done' WHERE ticket_id = '${boughtTicketId}';`
+    console.log("sql: ", sql);
+    con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("result: ", result);
       resolve(result);
     });    
   }) 
 }
+
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+
+
 
 
