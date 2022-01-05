df60ae61f74d2d03c9230ca1ec8c65f0c4347665
Sheng-yu Tsai <uopsdod@gmail.com>
2022-01-05 00:41:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function update_db(boughtTicketId, agendaProvider) {
       console.log("bank 15%, host 45%, subhost 25%, platform 15%");
     }
     
-    // 可優化事項: 判斷如果此 boughtTicketId 狀態以為 done，則不再重複處理
+    // 可優化事項: 判斷如果此 boughtTicketId 狀態已為 done，則不再重複處理
     
     let sql = `UPDATE ticketsystem.ticket SET ticket_status = 'done' WHERE ticket_id = '${boughtTicketId}';`
     console.log("sql: ", sql);
